53e7801814a3c7db855326c36bc1bcdd12688be3
Serhii <nikiforenko.sergiy@gmail.com>
2023-06-06 14:29:45 +0300

    Categories design

--- client/src/data.js
+++ client/src/data.js
@@ -4,24 +4,45 @@ import img3 from "./components/images/3.png";
 
 export const sliderItems = [
     {
-      id: 1,
-      img: img1,
-      title: "SUMMER SALE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "f5fafd",
+        id: 1,
+        img: img1,
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
     },
     {
-      id: 2,
-      img: img2,
-      title: "AUTUMN COLLECTION",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "fcf1ed",
+        id: 2,
+        img: img2,
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
     },
     {
-      id: 3,
-      img: img3,
-      title: "LOUNGEWEAR LOVE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "fbf0f4",
+        id: 3,
+        img: img3,
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "SHIRT STYLE!",
+        cat:"women"
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "LOUNGEWEAR LOVE",
+        cat:"coat"
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        title: "LIGHT JACKETS",
+        cat:"jeans"
     },
 ];
