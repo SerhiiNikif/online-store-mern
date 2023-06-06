152538ae63e79db889d3e8cb9409b3ba8701b668
Serhii <nikiforenko.sergiy@gmail.com>
2023-06-06 18:14:21 +0300

    USER: get all, get/:id, delete/:id, get stats; verifyTokenAndAdmin

--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -23,6 +23,20 @@ const verifyTokenAndAuthorization = (req, res, next) => {
             res.status(403).json("You are not alowed to do that!");
         }
     })
-}
+};
 
-module.exports = {verifyToken, verifyTokenAndAuthorization}
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.isAdmin) {
+            next();
+        } else {
+            res.status(403).json("You are not alowed to do that!");
+        }
+    });
+};
+
+module.exports = {
+    verifyToken, 
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin
+}
