5b4ea58f8fce0cfa4c7b3cac8fd51fe943c79b91
Serhii <nikiforenko.sergiy@gmail.com>
2023-06-06 16:11:57 +0300

    MongoDB models, Routing

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+const userRoute = require("./routes/user")
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,5 +11,8 @@ mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log('DB Connection Successfull!'))
     .catch(err => console.log(err))
+
+app.use(express.json());
+app.use("/api/user", userRoute);
 
 app.listen(PORT, () => console.log('Backend server is running on port', PORT));
