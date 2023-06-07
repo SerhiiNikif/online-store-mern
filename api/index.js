2639960179092541b57b827fa6d66f65e9a0f5a5
Serhii <nikiforenko.sergiy@gmail.com>
2023-06-07 10:18:05 +0300

    Api for products

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,5 +17,6 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 app.listen(PORT, () => console.log('Backend server is running on port', PORT));
