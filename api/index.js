a88e840e3a4e31d83041362e1fe1790b09be1c5f
Serhii <nikiforenko.sergiy@gmail.com>
2023-06-07 12:29:04 +0300

    Stripe Payment - Interaction of API and React

--- api/index.js
+++ api/index.js
@@ -8,6 +8,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,11 +18,13 @@ mongoose
     .then(() => console.log('DB Connection Successfull!'))
     .catch(err => console.log(err))
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(PORT, () => console.log('Backend server is running on port', PORT));
--- client/src/pages/Pay.jsx
+++ client/src/pages/Pay.jsx
@@ -1,11 +1,33 @@
 import StripeCheckout from "react-stripe-checkout";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router";
 
 const KEY = "pk_test_51NDRrwFB6F7Pi0esz6SpiWYLrIiYtbCfmOXANSGId6OAt3xPIaBCPJfxkQkQH11MI00DNbdwIm8M9E3qKd1pNcdi00WcU6F7nN";
 
 const Pay = () => {
-    const onToken = (token) => {
-        console.log(token);
-    };
+    const [stripeToken, setStripeToken] = useState(null);
+    const history = useHistory();
+
+    const onToken = token => setStripeToken(token);
+
+    useEffect(() => {
+        const makeRequest = async () => {
+            try {
+                const res = await axios.post(
+                    "http://localhost:5000/api/checkout/payment", {
+                        tokenId: stripeToken.id,
+                        amount: 2000
+                    }
+                );
+                console.log(res.data);
+                history.push("/success");
+            } catch(err) {
+                console.log(err);
+            }
+        };
+        stripeToken && makeRequest()
+    }, [stripeToken, history]);
 
     return (
         <div
@@ -16,31 +38,33 @@ const Pay = () => {
                 justifyContent: "center"
             }}
         >
-            <StripeCheckout 
-                name="Lama Shop" 
-                image="https://avatars.githubusercontent.com/u/1486366?v=4"
-                billingAddress
-                shippingAddress
-                description="Your total is $20"
-                amount={20 * 100}
-                token={onToken}
-                stripeKey={KEY}
-            >
-                <button
-                    style={{
-                        border: "none",
-                        width: 120,
-                        borderRadius: 5,
-                        padding: "20px",
-                        backgroundColor: "black",
-                        color: "white",
-                        fontWeight: "600",
-                        cursor: "pointer"
-                    }}
+            {stripeToken ? (<span>Processing. Please wait...</span>) : (
+                <StripeCheckout 
+                    name="Lama Shop" 
+                    image="https://avatars.githubusercontent.com/u/1486366?v=4"
+                    billingAddress
+                    shippingAddress
+                    description="Your total is $20"
+                    amount={20 * 100}
+                    token={onToken}
+                    stripeKey={KEY}
                 >
-                    Pay Now
-                </button>
-            </StripeCheckout>
+                    <button
+                        style={{
+                            border: "none",
+                            width: 120,
+                            borderRadius: 5,
+                            padding: "20px",
+                            backgroundColor: "black",
+                            color: "white",
+                            fontWeight: "600",
+                            cursor: "pointer"
+                        }}
+                    >
+                        Pay Now
+                    </button>
+                </StripeCheckout>
+            )}
         </div>
     )
 }
