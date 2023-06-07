8ec65c2245a80e7897c09f08a123ba61d8c88dca
Serhii <nikiforenko.sergiy@gmail.com>
2023-06-07 14:37:17 +0300

    Select Category and route to CategoryItem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,38 @@
-import Cart from "./pages/Cart";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
 import Product from "./pages/Product";
+import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
-
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Pay from "./pages/Pay";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import { 
+    BrowserRouter as Router, 
+    Switch, 
+    Route,
+    Redirect
+} from "react-router-dom";
 
 const App = () => {
+    const user = true;
     return (
         <Router>
             <Switch>
-                <Route path="/pay">
-                    <Pay />
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/products/:category">
+                    <ProductList />
+                </Route>
+                <Route path="/product/:id">
+                    <Product />
+                </Route>
+                <Route path="/cart">
+                    <Cart />
+                </Route>
+                <Route path="/login">
+                    {user ? <Redirect to="/"/> : <Login />}
+                </Route>
+                <Route path="/register">
+                    {user ? <Redirect to="/"/> : <Register />}
                 </Route>
             </Switch>
         </Router>
--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -48,8 +48,8 @@ const ProductList = () => {
             <FilterContainer>
                 <Filter>
                     <FilterText>Filter Products:</FilterText>
-                    <Select>
-                        <Option disabled selected>Color</Option>
+                    <Select defaultValue={"Color"}>
+                        <Option disabled>Color</Option>
                         <Option>white</Option>
                         <Option>black</Option>
                         <Option>red</Option>
@@ -58,8 +58,8 @@ const ProductList = () => {
                         <Option>green</Option>
                     </Select>
 
-                    <Select>
-                        <Option disabled selected>Size</Option>
+                    <Select defaultValue={"Size"}>
+                        <Option disabled>Size</Option>
                         <Option>XS</Option>
                         <Option>S</Option>
                         <Option>M</Option>
@@ -70,8 +70,8 @@ const ProductList = () => {
 
                 <Filter>
                     <FilterText>Sort Products:</FilterText>
-                    <Select>
-                        <Option selected>Newest</Option>
+                    <Select defaultValue={"Newest"}>
+                        <Option disabled>Newest</Option>
                         <Option>Price (asc)</Option>
                         <Option>Price (desc)</Option>
                     </Select>
